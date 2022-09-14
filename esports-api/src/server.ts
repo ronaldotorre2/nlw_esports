aa87465d3b37714dd7ff3b9990ec58461af6f0f3
Ronaldo Torre <ronaldo.torre@outlook.com>
2022-09-14 12:16:02 -0300

    Api: Add prisma

--- esports-api/src/server.ts
+++ esports-api/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.status(200).json();
+})
+
 app.listen(3333, () => {
     console.log('Hello!..');
     console.log('Api is runing!');
